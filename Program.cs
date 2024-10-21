97b92ad5f60a16621e7c057bbfe2d091e7468afe
USER <USER@LAPTOP-MAI9O57L>
2024-10-21 07:56:37 -0700

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿namespace EjemplosWwhile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int repetir = 100;
+            int i = 100; //este es un contador 
+
+            while (i < repetir)
+            {
+                i++;
+                Console.WriteLine("Bienvenido  i =" + 1);
+            }
+            do
+            {
+                i++;
+                Console.WriteLine("Biemvenido  i=" + i)
+
+             } while (i < repetir);
+
+
+
+        }
+    }
+}
